afdcc21fcc4cda8db11f756ccb42aeb73eb65fba
Fawaz <abdulsalamfawaz4@gmail.com>
2021-01-24 20:38:26 +0100

    fixing

--- scholars_/settings/prod.py
+++ scholars_/settings/prod.py
@@ -5,5 +5,5 @@ prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 DEBUG = False
 ALLOWED_HOSTS = ['scholarsb.herokuapp.com']
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
