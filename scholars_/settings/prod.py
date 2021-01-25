5eec367d2e647dce8a4c26ea40408d11a720f59b
Fawaz <abdulsalamfawaz4@gmail.com>
2021-01-25 08:25:50 +0100

    oya

--- scholars_/settings/prod.py
+++ scholars_/settings/prod.py
@@ -3,12 +3,13 @@ from .base import *
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['scholarsb.herokuapp.com']
 # STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR,'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
